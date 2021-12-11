ad7ca9263abe29b85dbabd3fab3af722c27d996a
ManliestBen <benjamintmanley@gmail.com>
2021-12-11 14:15:00 -0600

    refactor back end to .then

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,20 +2,35 @@ import { User } from '../models/user.js'
 import { Profile } from '../models/profile.js'
 import jwt from 'jsonwebtoken'
 
-async function signup(req, res) {
-  const profile = new Profile(req.body)
-  req.body.profile = profile._id
-  const user = new User(req.body)
-  try {
-    await user.save();
-    await profile.save();
+// async function signup(req, res) {
+//   const profile = new Profile(req.body)
+//   req.body.profile = profile._id
+//   const user = new User(req.body)
+//   try {
+//     await user.save();
+//     await profile.save();
 
-    const token = createJWT(user)
-    res.json({ token })
+//     const token = createJWT(user)
+//     res.json({ token })
   
-  } catch (err) {
+//   } catch (err) {
+//     res.status(400).send({ err: err.errmsg })
+//   }
+// }
+
+function signup(req, res) {
+  Profile.create(req.body)
+  .then(profile => {
+    req.body.profile = profile._id
+    User.create(req.body)
+    .then(user => {
+      const token = createJWT(user)
+      res.json({ token })
+    })
+  })
+  .catch(err => {
     res.status(400).send({ err: err.errmsg })
-  }
+  })
 }
 
 function createJWT(user) {
@@ -26,9 +41,27 @@ function createJWT(user) {
   )
 }
 
-async function login(req, res) {
-  try {
-    const user = await User.findOne({ email: req.body.email })
+// async function login(req, res) {
+//   try {
+//     const user = await User.findOne({ email: req.body.email })
+//     if (!user) return res.status(401).json({ err: 'User not found'})
+//     user.comparePassword(req.body.pw, (err, isMatch) => {
+//       if (isMatch) {
+//         const token = createJWT(user)
+//         res.json({ token })
+//       } else {
+//         return res.status(401).json({ err: 'Incorrect password' })
+//       }
+//     })
+//   }
+//   catch (err) {
+//     return res.status(400).json(err)
+//   }
+// }
+
+function login(req, res) {
+  User.findOne({ email: req.body.email })
+  .then(user => {
     if (!user) return res.status(401).json({ err: 'User not found'})
     user.comparePassword(req.body.pw, (err, isMatch) => {
       if (isMatch) {
@@ -38,10 +71,10 @@ async function login(req, res) {
         return res.status(401).json({ err: 'Incorrect password' })
       }
     })
-  }
-  catch (err) {
-    return res.status(400).json(err)
-  }
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  })
 }
 
 export {
