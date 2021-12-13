864eb6d3e792ac04601d58b6e44cb69f1abbea18
David <49245298+DavidStinson@users.noreply.github.com>
2021-12-12 19:33:56 -0600

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,3 @@
-import * as tokenService from './tokenService'
 const BASE_URL = '/api/auth/'
 
 async function signup(user) {
@@ -10,14 +9,16 @@ async function signup(user) {
     })
     const json = await res.json()
     if (json.token) {
-      tokenService.setToken(json.token)
+      return json.token
     }
     if (json.err) {
       throw new Error(json.err)
     }
   } catch (err) {
+    console.log(err)
     throw err
   }
 }
 
 export { signup, }
+
