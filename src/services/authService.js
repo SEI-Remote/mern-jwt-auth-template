ef370c902f27aebadce2bf8c6f79bbfb1bb1e13a
ManliestBen <benjamintmanley@gmail.com>
2021-12-28 22:08:15 -0600

    implement password change functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -47,4 +47,27 @@ async function login(credentials) {
   }
 }
 
-export { signup, getUser, logout, login }
+async function changePassword(credentials) {
+  try {
+    const res = await fetch(`${BASE_URL}/changePassword`, {
+      method: 'POST',
+      headers: new Headers({ 
+        'Content-Type': 'application/json', 
+        Authorization: `Bearer ${tokenService.getToken()}` 
+      }),
+      body: JSON.stringify(credentials),
+    })
+    const json = await res.json()
+    if (json.token) {
+      tokenService.removeToken()
+      tokenService.setToken(json.token)
+    }
+    if (json.err) {
+      throw new Error(json.err)
+    }
+  } catch(err) {
+    throw err
+  }
+}
+
+export { signup, getUser, logout, login, changePassword }
